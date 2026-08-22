c7bf90d63e034c7fa3390ffea92f18480eada53b
Mihaela Durdevic <mihaeladurdevic@Mihaelas-MacBook.local>
2026-08-22 14:15:55 +0200

    implement geocording fetch for city search

--- script.js
+++ script.js
@@ -5,10 +5,30 @@ function init() {
     document.body.classList.add("dark");
   }
   drakToggle.addEventListener("click", toggleDarkMode);
+
+  const searchForm = document.querySelector(".weather-app__search-button");
+  searchForm.addEventListener("click", async function handleSearch(event) {
+    let searchInputRef = document.getElementById("search-input");
+    let searchInput = searchInputRef.value;
+    const result = await getWeatherData(searchInput);
+    console.log(result);
+  });
 }
 
 function toggleDarkMode() {
   document.body.classList.toggle("dark");
   let theme = document.body.classList.contains("dark") ? "dark" : "light";
   localStorage.setItem("theme", theme);
+}
+
+async function getWeatherData(city) {
+  try {
+    const fetchResponse = await fetch(
+      `https://geocoding-api.open-meteo.com/v1/search?name=${encodeURIComponent(city)}`,
+    );
+    const data = await fetchResponse.json();
+    return data;
+  } catch (error) {
+    console.error("Fehler beim Laden:", error);
+  }
 }
